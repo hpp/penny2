8e4392b6ac923eb934fabe9fff34f8c80e61ad97
Joe Walter <harmonicproc@gmail.com>
2015-07-24 17:32:17 -0600

    Create ReadMe.m

---
+++ ReadMe.m
@@ -0,0 +1,11 @@
+This project is no longer opensource in the strictor sense of the term. 
+This project demonstrates how to use Harmonic Processes Inc., HPP's texture 
+audio processing framework. 
+
+
+ "We claim for our invention a utility or otherwise how be it patentable system for 
+ using particle filters to model surface transitions and resonant timbres using 
+ textures or lookup tables of nodal or other acoustical areas of interest in 2d or 3d."
+ 
+ 
+ All rights reserved.
